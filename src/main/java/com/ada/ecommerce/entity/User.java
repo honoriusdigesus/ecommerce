bda40a13faecf272e675b2c22c2bd0770aef7510
Honorius di Gesús <honoriusdigesus@gmail.com>
2024-01-28 18:47:04 -0500

    Configurando la clase user para que se comporte como un UserDetails

--- src/main/java/com/ada/ecommerce/entity/User.java
+++ src/main/java/com/ada/ecommerce/entity/User.java
@@ -7,40 +7,81 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
 import java.sql.Types;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
+
 import lombok.Data;
 import org.hibernate.annotations.JdbcTypeCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Data
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
-  @Id
-  @GeneratedValue
-  @JdbcTypeCode(Types.VARCHAR)
-  private UUID id;
+    @Id
+    @GeneratedValue
+    @JdbcTypeCode(Types.VARCHAR)
+    private UUID id;
 
-  @Column(name = "first_name",nullable = false, unique = true, length = 30)
-  private String firstName;
+    @Column(name = "first_name", nullable = false, unique = true, length = 30)
+    private String firstName;
 
-  @Column(name = "last_name",nullable = false, unique = false, length = 30)
-  private String lastName;
-
-
-  @Column(name = "email",nullable = false, unique = true, length = 30)
-  private String email;
+    @Column(name = "last_name", nullable = false, unique = false, length = 30)
+    private String lastName;
 
 
-  @Column(name = "password",nullable = false, unique = true, length = 30)
-  private String password;
+    @Column(name = "email", nullable = false, unique = true, length = 30)
+    private String email;
 
-  private String address;
-  private Boolean enable= false;
 
-  @ManyToOne
-  @JoinColumn(name = "role_id")
-  private Role role;
+    @Column(name = "password", nullable = false, unique = true, length = 30)
+    private String password;
 
+    private String address;
+    private Boolean enable = false;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(role.getName());
+        return List.of(authority);
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enable;
+    }
 }
--- src/main/java/com/ada/ecommerce/services/ProductService.java
+++ src/main/java/com/ada/ecommerce/services/ProductService.java
@@ -7,11 +7,11 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-public interface ProductService {
+public interface  ProductService {
   Product save(ProductDTO product);
   Product getById(UUID uuid);
 
   Page<Product> getProduct(Pageable pageable); //Obtener productos paginados
-  PageDTO<Product> getProductsFiltered(Double minPrice, Double maxPrice,Pageable pageable); //Obtener productos filtrados
+  PageDTO<Product> getProductsFiltered(Double minPrice, Double maxPrice,Pageable pageable); //Obt ener productos filtrados
 
 }
